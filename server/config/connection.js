e0e9d4f54b1d1d020a4b0c613f373ea7c9f8e312
Blake Johnson <blakejohnson@Blakes-MacBook-Pro.local>
2021-08-29 19:17:48 -0400

    leaving branch to start adding components

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rescueexpress', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rex2', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
